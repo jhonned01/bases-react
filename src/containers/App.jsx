0146e9caccf94b8ac6aafe39892f1d50bf0818d0
jhonned01 <jhonned01@gmail.com>
2020-09-21 23:12:46 -0500

    terminado a medias

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from  'react'
+import React,{useState,useEffect} from  'react'
 import Header from '../components/Header'
 import '../assets/syles/App.scss';
 import Search from '../components/Search'
@@ -6,34 +6,45 @@ import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarrouselIteam from '../components/CarouselIteam'
 import Footer from '../components/Footer'
+import useInitialState from '../assets/hooks/useInitialState'
 
-const App = () => (
-    <div className="App">
+
+    const API='http://localhost:3000/initialState'
+    
+const App = () => {
+   const initialState =useInitialState(API)
+    
+    return (
+    <div className="App" >
         <Header />
         <Search />
-
-        <Categories title="prueba">
+        {initialState.mylist.length > 0 && (
+        <Categories title="MI LISTA">
             <Carousel>
-                <CarrouselIteam />
-                <CarrouselIteam />
-                <CarrouselIteam />
-                <CarrouselIteam />
+             <CarrouselIteam />
+            
             </Carousel>          
         </Categories>
 
-        <Categories title="prueba2">
+        )}
+
+        
+
+        <Categories title="TENDENCIAS">
             <Carousel>
-                <CarrouselIteam />
-                <CarrouselIteam />
-                <CarrouselIteam />
-                <CarrouselIteam />
+                {initialState.trends.map(iteam =>
+                 
+                 <CarrouselIteam key={iteam.id} {...iteam}/>
+                )}
+               
+               
             </Carousel>          
         </Categories>
 
         <Footer />
     </div>
 )
-
+    }
 
 
 export default App;
